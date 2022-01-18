a30617e8a0c494e349a10df30270955b103d5af0
3gabyxD <gaby3xd@gmail.com>
2022-01-18 21:25:10 +0800

    Implemented moving window

--- src/FX-Tools/Timeline.lua
+++ src/FX-Tools/Timeline.lua
@@ -6,17 +6,19 @@ local Instantiate = require(script.Parent.Utility.Instantiate)
 local colorize = require(script.Parent.Utility.colorize)
 
 --|| Templates ||--
-local window = require(script.Parent.Elements.window)
+local movingWindow = require(script.Parent.Elements.movingWindow)
 
 --|| Timeline ||--
 local timeline = {}
 function timeline.new(parent, config)
 	local self = setmetatable({
-		Window = window.new(
+		Window = movingWindow.new(
 			parent,
-			Vector2.new(1, 0),
-			UDim2.fromOffset(200, 390)
-		)
+			Vector2.new(.5, .5),
+			UDim2.fromScale(.5, .5),
+			UDim2.fromOffset(450, 250)
+		),
+		Config = config
 	}, {__index = timeline})
 
 	return self
